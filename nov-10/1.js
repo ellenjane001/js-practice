8e6616855e3d55951291ec3f7022f5466cc08fa1
ellenjane001 <ellenjane.000001998@gmail.com>
2022-11-10 21:01:15 +0800

    added math functions

---
+++ nov-10/1.js
@@ -0,0 +1,12 @@
+let r = 9
+let area = Math.PI * r * r
+console.log(area)
+
+
+let n = 5.000
+
+console.log(Math.ceil(-5.0000))
+console.log(Math.floor(n))
+console.log(Math.round(5.6868))
+
+console.log(Math.random())
---
+++ nov-10/2.js
@@ -0,0 +1,7 @@
+let throwcount, dicethrow
+
+
+for (throwcount = 0; throwcount < 10; throwcount++)
+{
+    dicethrow = Math.floor()
+}
