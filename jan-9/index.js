8764b70c21d6893f7ef3c0a4a6fb5e9f97a49798
ellenjane001 <ellenjane.000001998@gmail.com>
2023-01-09 19:48:23 +0800

    added january 9 practices

---
+++ jan-9/index.js
@@ -0,0 +1,8 @@
+const misc = require('./misc')
+// import { divide } from './misc'
+
+console.log(misc.divide(56, 5))
+console.log(misc.subtract(56, 5))
+console.log(misc.multiply(56, 5))
+
+// console.log(divide(12, 6))
---
+++ jan-9/misc.js
@@ -0,0 +1,15 @@
+function divide(a, b) {
+    let q = a / b
+    return q
+}
+
+function multiply(a, b) {
+    let m = a * b
+    return m
+}
+
+function subtract(a, b) {
+    return a - b
+}
+
+module.exports = { divide, subtract, multiply }
