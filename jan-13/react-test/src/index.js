3e750ad891e86ec5a9dd8d1af5d852730184b91b
ellenjane001 <ellenjane.000001998@gmail.com>
2023-01-14 11:57:56 +0800

    added jan 14 code along

--- jan-13/react-test/src/index.js
+++ jan-13/react-test/src/index.js
@@ -5,11 +5,24 @@ const el = document.getElementById('root')
 const root = ReactDOM.createRoot(el)
 
 function App() {
+    let message = 'hello there'
+    if (Math.random() > 0.5)
+        message = 'good bye'
+    let inputType = 'number'
+    let minValue = 5
     return (
         <div>
             <h1>Hello World</h1>
-            <label>Test</label>
-            <input type="text" />
+            <h1>{message}</h1>
+            <p>{5 + 3}</p>
+            <hr />
+            <div>
+                <input type={inputType} min={minValue} />
+                <input type="number" min={3} />
+            </div>
+            <p>
+                <input type="number" min={3} max={10} list={[9, 8, 7]} style={{ color: 'red', borderColor: "pink" }} alt={message} />
+            </p>
         </div>
     )
 }
