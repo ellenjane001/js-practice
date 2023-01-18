f091aff6d62f8f1d1af277685b77143b0ce72b3c
ellenjane001 <ellenjane.000001998@gmail.com>
2023-01-18 23:14:06 +0800

    added jan 18 code along

---
+++ jan-18/src/GadgetShow.js
@@ -0,0 +1,11 @@
+function GadgetShow(props) {
+    const { type } = props
+    return (<div>
+        <h1>
+            GadgetShow
+        </h1>
+        <h2>{type}</h2>
+    </div>)
+}
+
+export default GadgetShow
---
+++ jan-18/src/MyApp.js
@@ -0,0 +1,27 @@
+import { Button } from "@mui/material"
+import { useState } from "react"
+import GadgetShow from "./GadgetShow"
+
+const getRandomGadget = () => {
+    const gadgets = ['laptop', 'phone', 'watch', 'ipad', 'e-bike']
+    return gadgets[Math.floor(Math.random() * gadgets.length)]
+}
+
+function App() {
+    const [count, setCount] = useState(0)
+    const [gadgets, setGadgets] = useState([])
+    const gadgetsForRendering = gadgets.map((gadget, index) => {
+        return <GadgetShow type={gadget} key={index} />
+    })
+    const handleClick = () => {
+        setCount(prevCount => prevCount + 1)
+        setGadgets([...gadgets, getRandomGadget()])
+    }
+    return (<div className='App'>
+        <h1>{count}</h1>
+        <p>{gadgetsForRendering}</p>
+        <Button variant="contained" onClick={handleClick}>Generate</Button>
+    </div>)
+}
+
+export default App
