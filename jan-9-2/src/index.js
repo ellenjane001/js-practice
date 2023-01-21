b1871a110de37462a3c0fc5aae6e9121dcdba48e
ellenjane001 <ellenjane.000001998@gmail.com>
2023-01-21 11:34:09 +0800

    finalize

--- jan-9-2/src/index.js
+++ jan-9-2/src/index.js
@@ -13,7 +13,7 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
-    res.send('Hello WD19P learners')
+    // res.send('Hello WD19P learners')
 })
 
 app.get('/test', (req, res) => {
